e04237b24e4d0fbc69077447ac36c3ed22fc518b
catVSdog <2318365421@qq.com>
2019-12-18 17:25:23 +0800

    eslint config

--- server_source/app.ts
+++ server_source/app.ts
@@ -3,23 +3,22 @@ import * as Koa from "koa";
 import * as KoaRouter from "koa-router";
 
 class ManagerApp {
-     app: Koa;
-     router: KoaRouter;
+    app: Koa;
+    router: KoaRouter;
 
-     constructor() {
-      this.app = new Koa();
-      this.router = new KoaRouter();
+    constructor() {
+        this.app = new Koa();
+        this.router = new KoaRouter();
     }
 
     configApp() {
-      this.app.use(this.router.routes());
+        this.app.use(this.router.routes());
     }
 }
 
-
 const managerApp = new ManagerApp();
 
-const app  = managerApp.app;
+const app = managerApp.app;
 
 managerApp.configApp();
 export { ManagerApp, app };
