61a7c74cd6a2ea56c127af2e421533fb882a122f
catVSdog <2318365421@qq.com>
2019-12-20 12:43:50 +0800

    webpack success

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,42 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const tsImportPluginFactory = require('ts-import-plugin');
+
 module.exports = {
     mode: 'development',
     devtool: 'eval-source-map',
-    entry: "./client_source/index.ts",
+    entry: './client_source/index.ts',
     output: {
         path: path.resolve(__dirname, './client_dist'),
-        publicPath: "/",
-        filename: "[name].js"
+        publicPath: '/',
+        filename: '[name].js'
     },
     resolve: {
-        extensions: [".ts", ".js", ".tsx"],
+        extensions: ['.ts', '.js', '.tsx']
     },
     module: {
         rules: [
             {
                 test: /\.vue$/,
-                loader: 'vue-loader',
+                loader: 'vue-loader'
             },
             {
                 test: /\.ts(\?.*)?$/,
                 loader: 'ts-loader',
-                exclude: '/node_modules',
-
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                    transpileOnly: true
+                }
             }
         ]
     },
     plugins: [
         new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './client_source/index.html'
+        })
     ]
 };
