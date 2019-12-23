ae8f3df74d37f0c7f75ae1780025377e83b1249a
catVSdog <windorbird@gmail.com>
2019-12-23 20:47:44 +0800

    增加css

--- client_source/components/App.vue
+++ client_source/components/App.vue
@@ -4,13 +4,24 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { Container } from 'element-ui';
+import { Container, Footer, Main, Header, Aside } from 'element-ui';
 import VueRouter from 'vue-router';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(Container);
+Vue.use(Footer);
+Vue.use(Main);
+Vue.use(Header);
+Vue.use(Aside);
 Vue.use(VueRouter);
 
 export default {
     name: 'App'
 };
 </script>
+
+<style lang="scss">
+* {
+    box-sizing: border-box;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,21 @@ module.exports = {
                     appendTsSuffixTo: [/\.vue$/],
                     transpileOnly: true
                 }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'url-loader'
             }
         ]
     },
